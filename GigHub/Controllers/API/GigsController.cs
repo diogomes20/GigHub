b297dd4da949286362310afef96f21b72800f8da
João Pacheco <joao.pacheco@inelcis.pt>
2019-10-10 15:05:50 +0100

    Insert notifications

--- GigHub/Controllers/API/GigsController.cs
+++ GigHub/Controllers/API/GigsController.cs
@@ -24,7 +24,34 @@ namespace GigHub.Controllers.API
         {
             var userId = User.Identity.GetUserId();
             var gig = _context.Gigs.Single(g => g.Id == id && g.ArtistId == userId);
+
+            if (gig.IsCanceled)
+                return NotFound();
+
             gig.IsCanceled = true;
+
+            var notification = new Notification
+            {
+                DateTime = DateTime.Now,
+                Gig = gig,
+                Type = NotificationType.GigCanceled
+            };
+
+            var attendees = _context.Attendances
+                .Where(a => a.GigId == gig.Id)
+                .Select(a => a.Attendee)
+                .ToList();
+
+            foreach (var atendee in attendees)
+            {
+                var userNotification = new UserNotification
+                {
+                    User = atendee,
+                    Notification = notification
+                };
+                _context.UserNotifications.Add(userNotification);
+            }
+
             _context.SaveChanges();
 
             return Ok();
